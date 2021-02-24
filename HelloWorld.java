315707b9fd42628396b08ab745bf2fda87a7147e
Seifu Birega <50331075+SeifuB@users.noreply.github.com>
2021-02-24 06:29:09 +0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/*
+This is my first
+Java program
+
+*/
+public class HelloWorld {
+    
+    public static void main(String[] Args){
+        
+             
+        System.out.println("Hello Mr. Seifu, you programming result is:");
+        System.out.println("");
+
+        
+        int x=20;
+        int y=8;
+        
+        int Z=x+y;
+        int W=x-y;
+        int A=x*y;
+        int B= x%y;
+        
+         if (x > y){
+      
+        System.out.println(x+" is graterthan "+y);
+                 System.out.println("");
+
+         }
+         
+         else if(x < y){
+     
+             System.out.println(x+" is lessthan "+y);
+             System.out.println("");
+
+         }
+     
+         else {
+             
+        System.out.println(x+" is equal to "+y);
+              System.out.println("");
+  
+        }  
+         
+         // i++ = i+1
+         //
+         
+         for(int i=0; i<=5; i++){
+            
+             //System.out.println("");
+             System.out.println("i="+i);
+         }
+        
+        System.out.println("");
+        System.out.println("Z="+Z);
+        System.out.println("W="+W);
+        System.out.println("A="+A);
+        System.out.println("B="+B);
+        
+        
+        
+    }      
+}
